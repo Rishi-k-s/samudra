a7d9cbe8f287430b637ad207a0adc40e386b2637
Rishi-k-s <rishikrishna.sr@gmail.com>
2024-02-23 21:50:32 +0530

    started to add ar core

---
+++ lib/arpage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class arPage extends StatefulWidget {
+  const arPage({super.key});
+
+  @override
+  State<arPage> createState() => _arPageState();
+}
+
+class _arPageState extends State<arPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
